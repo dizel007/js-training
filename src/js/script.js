f5d4b80129c9704d9a62199c1c9922c029642a5c
Dima <dizel007@yandex.ru>
2019-11-26 11:02:13 +0500

    Динамическое изменении типов данных
    внизу файла примеры перевода в строку, в числа, в булинг

--- src/js/script.js
+++ src/js/script.js
@@ -88,7 +88,7 @@ let appData = {
 
 for (let key in appData) {
     console.log("Наша программа включает в себя данные: " + key + " - " + appData[key]);
-}
+} 
 
 
 
@@ -224,3 +224,54 @@ for (let key in appData) {
 //     i++;
 // }
 // while(i < 2);
+
+
+
+
+/// перевод данных в строку
+
+// 1 
+console.log(typeof(String(null)));
+console.log(typeof(String(4)));
+
+// 2  конконтинация
+console.log(typeof("ww" + 5));
+
+console.log('https:\\vk.com/catalog' + 5);
+
+/// преобразование данных в числа
+//1
+console.log(typeof(Number('5')));
+//2
+console.log(typeof(+'5')); // унарный плюс
+console.log(typeof(5 + +'5'));
+
+//3 
+console.log(typeof(parseInt('15px', 10)));
+console.log(parseInt('15px', 10));
+
+
+let ans = +prompt(" Hello& ", '');
+
+/// логическое преобразование
+
+//false = 0, '', null, undefine, NaN
+//true = ' ', 
+// 1
+let switcher = null;
+
+if (switcher) {
+    console.log("Working");
+}
+switcher = 1;
+if (switcher) {
+    console.log("Working.....");
+}
+// 2 
+console.log(typeof(Boolean('5')));
+// 3
+console.log(typeof(!!'5'));
+
+
+
+
